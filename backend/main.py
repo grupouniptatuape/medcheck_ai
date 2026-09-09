282ae58d814a671f2e5656713c4b8a03d3a790c6
camila <trabalho.meninas.unip@gmail..com>
2026-09-09 14:46:52 -0300

    Criando endpoint inicial de analise

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI(
     title="MedCheck AI",
     description="API backend do MedCheck AI",
     version="0.1.0"
 )
+// o que o sistema está recebendo: texto ou linkw
+class EntradaAnalise(BaseModel):
+    tipo_entrada: str
+    conteudo: str
+
 
 
 @app.get("/")
@@ -12,4 +18,13 @@ def home():
     return {
         "status": "ok",
         "message": "MedCheck AI backend funcionando"
+    }
+
+// teste de recebimento
+@app.post("/analisar")
+def analisar(entrada: EntradaAnalise):
+    return {
+        "tipo_entrada": entrada.tipo_entrada,
+        "conteudo": entrada.conteudo,
+        "mensagem": "Conteúdo recebido com sucesso"
     }
